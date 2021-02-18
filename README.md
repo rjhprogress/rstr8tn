74e7de82d22ab0d2c63a2bcaf66ccf23490b6172
Rasheedah aka Rocky <rjhpenndrive@gmail.com>
2021-02-17 21:28:41 -0500

    READ.me updates with files and directories

--- README.md
+++ README.md
@@ -1,23 +1,73 @@
 https://rjhprogress.github.io/rstr8tn/
 
-Files and Directories 
-client Folder
-- node_modules
-- public 
- - Collections
- - Dashboard
- - Join
- - Releases
- - index.html
- - {} manifest.json
- - robots.txt
 
->src
+Files and Directories [Currently being updated]
+RSTR8TN Main 
+```
+Client Folder
+    Node_modules
+        default files
+
+    Public
+        Collections
+        Dashboard
+        Join
+        Releases
+
+    Src
+        Components
+            Footer
+                index.js
+            Mission
+                index.js
+            Navbar
+                index.js
+
+        App.js
+        Blacklivesmatter.js
+        Cart.js
+        Categories.js
+        Certificate.js
+        Commit.js
+        Cover.js
+        fist.jpg
+        fistraise.png
+        Freestyle.js
+        Gallery.js
+        Header.js
+        index.css
+        index.js
+        join.css
+        Newarrivals.js
+        Projects.js
+        Stonewallriots.js
+        Test.js
+        w3.css
+
+    Server
+        Controllers
+            auth.js
+        Models
+            user.js
+        Node_Modules
+            default modules
+        Routes
+            auth.js
+        {}package-lock.json
+        {}package.json
+        server.js
+    .eslintcache
+    .gitignore
+    {}package-lock.json
+    {}package.json   
+    README.md
+    server.js
+           
+
+ index.html
+ {} manifest.json
+ robots.txt
+```
 
 
-.eslintcache
-.gitignore
-{}package-lock.json
-{}package.json
-README.md
-server.js
+
--- server.js
+++ server.js
@@ -1,77 +1,39 @@
-const express = require('express')
+const express = require("express");
+cont morgan = require('morgan')
+const cors = require('cors')
 const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const DashboardRoute = require('./routes/dashboard')
-const AuthRoute = require('./routes/auth')
+require('dotenv').config()
 
 
-
-
-const { ESRCH } = require("constants")
 const path = require("path");
-const PORT = process.env.PORT || 3001
-const collections = ['releases']
-const db = mongojs(mongodb, collections)
+const PORT = process.env.PORT || 3001;
 
-const app = express()
+const app = express();
 
+// Define API routes here
+const authRoutes = require('./routes/auth')
+
+//app middlewares
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use('/logins', express.static('logins'))
 
-const PORT = process.env.PORT || 3000
+// middleware
+app.use('/api', authRoutes);
 
-app.listen(PORT, () => {
-  console.log('Server is running on port ${PORT}')
-})
-
-
-
-app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-
-db.on('error', error => {
-  console.log('Database Error:', error);
-});
-
-app.post('/submit', ({ body }, res) => {
-  const release = body;
-
-  release.read = false;
-
-  db.favorites.save(release, (error, saved) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.send(saved);
-    }
-  });
-});
-
-app.get('/read', (req, res) => {
-  db.favorites.find({ read: true }, (error, found) => {
-    if (error)
-  })
-}
-)
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
+
+
+// Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", function (req, res) {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
-
-
-app.use('/api/employee", DashboardRoute')
